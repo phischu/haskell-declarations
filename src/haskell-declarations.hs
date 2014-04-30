bb6b294f575a4d7687317e5fabd50f98ed2447a4
phischu <pschuster@uni-koblenz.de>
2014-04-30 12:48:07 +0200

    actually parse

--- src/haskell-declarations.hs
+++ src/haskell-declarations.hs
@@ -7,6 +7,7 @@ import Language.Haskell.Names
 import Language.Haskell.Names.Interfaces
 import Language.Haskell.Exts.Extension
 import Language.Haskell.Exts.SrcLoc
+import Language.Haskell.Exts.Parser (fromParseResult)
 import Control.Monad
 import Control.Exception
 import Data.Version
@@ -34,7 +35,18 @@ main =
 version :: Data.Version.Version
 version = Data.Version.Version [7,6,3] []
 
-theTool :: Compiler.Simple NamesDB
+suffix :: String
+suffix = "declarations"
+
+data DeclarationsDB = DeclarationsDB
+
+instance IsDBName DeclarationsDB where
+    getDBName = undefined
+
+nameFilesExtension :: FilePath
+nameFilesExtension = "declarations"
+
+theTool :: Compiler.Simple (StandardDB DeclarationsDB)
 theTool =
   Compiler.simple
     "haskell-names"
@@ -42,8 +54,39 @@ theTool =
     knownLanguages
     knownExtensions
     compile
-    ["declarations"]
+    [nameFilesExtension]
+
+fixCppOpts :: CpphsOptions -> CpphsOptions
+fixCppOpts opts =
+  opts {
+    defines = ("__GLASGOW_HASKELL__", "706") : defines opts, -- FIXME
+    preInclude = "cabal_macros.h" : preInclude opts
+  }
+
+parse :: Language -> [Extension] -> CpphsOptions -> FilePath -> IO (HSE.Module HSE.SrcSpan)
+parse lang exts cppOpts file = do
+    parseresult <- parseFileWithCommentsAndCPP (fixCppOpts cppOpts) mode file
+    return (fmap srcInfoSpan (fst (fromParseResult parseresult)))
+  where
+    mode = defaultParseMode
+             { UnAnn.parseFilename   = file
+             , baseLanguage          = lang
+             , extensions            = exts
+             , ignoreLanguagePragmas = False
+             , ignoreLinePragmas     = False
+             }
 
 compile :: Compiler.CompileFn
-compile buildDir mbLang exts cppOpts pkgName pkgdbs deps files = do
-    print "hello world"
+compile builddirectory maybelanguage extensions cppoptions packagename packagedbs dependencies files = do
+  let language = fromMaybe Haskell98 maybelanguage
+
+  modules <- mapM (parse language extensions cppoptions) files
+
+  forM_ modules (\modul -> do
+
+    let HSE.ModuleName _ modulename = getModuleName modul
+        declarationsfilename = builddirectory </> toFilePath (fromString modulename) <.> nameFilesExtension
+
+    createDirectoryIfMissingVerbose silent True (dropFileName declarationsfilename)
+
+    print ("working on: " ++ show packagename))
